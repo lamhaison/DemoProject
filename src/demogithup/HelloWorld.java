7fc71fd5f362652b8730876c3a019a74893af8cd
lamhaison <lamhaison@gmail.com>
2013-06-23 22:12:50 +0700

    Thay dổi Project lần đầu tiên

---
+++ src/demogithup/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demogithup;
+
+/**
+ *
+ * @author LHS
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
